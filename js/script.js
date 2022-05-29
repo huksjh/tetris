a31c3764ce2ecf3e369e98bce39b44f919714d48
huks <wkdgur1209@gmail.com>
2022-05-29 23:04:30 +0900

    테트리스 수정

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 import BLOCKS from "./blocks.js";
 const playground = document.querySelector(".playground > ul");
+const gameText = document.querySelector(".gameText");
 const GAME_ROWS = 20;
 const GAME_COLS = 10;
 
@@ -10,10 +11,10 @@ let downInterval;
 let tempMovingItem;
 
 const movingItem = {
-    type: "ll",
+    type: "tree",
     direction: 0,
     top: 0,
-    left: 0,
+    left: 3,
 };
 
 init();
@@ -24,8 +25,8 @@ function init() {
     for (let i = 0; i < GAME_ROWS; i++) {
         prependNewLine();
     }
-
     renderBlocks();
+    //generateNewBlock();
 }
 
 function prependNewLine() {
@@ -42,7 +43,7 @@ function prependNewLine() {
 
 function renderBlocks(moveType = "") {
     const { type, direction, top, left } = tempMovingItem;
-    console.log(tempMovingItem);
+    // console.log(tempMovingItem);
     const movingBlocks = document.querySelectorAll(".moving");
     // 이동한 기록이 있는 객체 클래스 초기화
     movingBlocks.forEach((moving) => {
@@ -70,13 +71,14 @@ function renderBlocks(moveType = "") {
             break;
         }
     }*/
+    console.log(moveType);
 
     BLOCKS[type][direction].some((block) => {
         //console.log("block", block);
         const x = block[0] + left;
         const y = block[1] + top;
         //console.log("playground.childNodes[y]", playground.childNodes[y]);
-        //console.log(playground.childNodes[y].childNodes);
+
         const target = playground.childNodes[y] ? playground.childNodes[y].childNodes[0].childNodes[x] : null;
         //console.log("target", target);
 
@@ -86,13 +88,17 @@ function renderBlocks(moveType = "") {
             target.classList.add(type, "moving");
         } else {
             tempMovingItem = { ...movingItem };
+            if (moveType == "reTry") {
+                gameOverShow();
+                return false;
+            }
             setTimeout(() => {
-                console.log("setTimeout");
-                renderBlocks();
+                renderBlocks("reTry");
                 if (moveType === "top") {
                     seizeBlock();
                 }
             }, 0);
+
             return true;
         }
     });
@@ -102,6 +108,12 @@ function renderBlocks(moveType = "") {
     movingItem.direction = direction;
 }
 
+// 게임오버
+function gameOverShow() {
+    clearInterval(downInterval);
+    gameText.style.display = "flex";
+}
+
 // 블럭 고정 css 부여
 function seizeBlock() {
     const movingBlocks = document.querySelectorAll(".moving");
@@ -109,21 +121,53 @@ function seizeBlock() {
         moving.classList.remove("moving");
         moving.classList.add("seized");
     });
+
+    checkMatch();
+}
+
+// 라인 검사
+function checkMatch() {
+    const childNodes = playground.childNodes;
+    childNodes.forEach((child) => {
+        let matched = true;
+        // rows li 안에 ul li 자식들 구해서 돌기
+        // li 안에 seized 블럭 css 가 한개라도 없으면
+        // 라인 지우기 실패 처리
+        child.children[0].childNodes.forEach((li) => {
+            if (!li.classList.contains("seized")) {
+                matched = false;
+            }
+        });
+        // seized가 전부 있다면 라인 지우기
+        // 새로운 라인 생성
+        if (matched) {
+            child.remove();
+            prependNewLine();
+        }
+    });
+
     generateNewBlock();
 }
 
 function generateNewBlock() {
+    // clearInterval(downInterval);
+    // downInterval = setInterval(() => {
+    //     moveBlock("top", 1);
+    // }, duration);
+
     let blockArray = Object.entries(BLOCKS);
-    let rand_type = Math.floor(Math.random() * Object.entries(BLOCKS).length);
+    let rand_type = Math.floor(Math.random() * blockArray.length);
     movingItem.type = blockArray[rand_type][0];
     movingItem.top = 0;
     movingItem.left = 3;
+    movingItem.direction = 0;
     tempMovingItem = { ...movingItem };
     renderBlocks();
 }
 
 // 블럭검사 target 영역 검사해서 있으면 moving css 없으면 seized 부여 하기위한 검사
 function checkEmpty(target) {
+    console.log(target);
     if (!target || target.classList.contains("seized")) {
         return false;
     }
@@ -150,6 +194,14 @@ function changeDirection() {
     renderBlocks();
 }
 
+// 스페이스바 누를때
+function dropBlock() {
+    clearInterval(downInterval);
+    downInterval = setInterval(() => {
+        moveBlock("top", 1);
+    }, 10);
+}
+
 // 이벤트
 document.addEventListener("keydown", (e) => {
     // console.log(e.keyCode);
@@ -165,6 +217,10 @@ document.addEventListener("keydown", (e) => {
             break;
         case 40: //하단 - 다운
             moveBlock("top", 1);
+            break;
+
+        case 32: //스페이스바
+            dropBlock();
             break;
         default:
             break;
